2bb950d71f92c5b679ab0a970e638f9801db5035
sbalieva <147823207+sbalieva@users.noreply.github.com>
2024-02-06 14:34:31 +0200

    TagServiceImplTest

--- src/test/java/com/example/forum/services/TagServiceImplTest.java
+++ src/test/java/com/example/forum/services/TagServiceImplTest.java
@@ -1,4 +1,47 @@
 package com.example.forum.services;
+import com.example.forum.models.Tag;
+import com.example.forum.repositories.TagRepositoryImpl;
+import com.example.forum.services.TagServiceImpl;
+import com.example.forum.utils.TagFilterOptions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 public class TagServiceImplTest {
+
+    @Mock
+    private TagRepositoryImpl tagRepository;
+
+    @InjectMocks
+    private TagServiceImpl tagService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void get_all_tags_with_no_filters() {
+        List<Tag> expectedTags = new ArrayList<>();
+        expectedTags.add(new Tag());
+        expectedTags.add(new Tag());
+
+        when(tagRepository.getAllTags(null)).thenReturn(expectedTags);
+
+        List<Tag> actualTags = tagService.getAllTags(null);
+
+        assertEquals(expectedTags.size(), actualTags.size());
+        for (int i = 0; i < expectedTags.size(); i++) {
+            assertEquals(expectedTags.get(i), actualTags.get(i));
+        }
+    }
+
 }
